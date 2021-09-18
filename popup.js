59b5ce1ed15dc8daa14f620e2cf48164f3d7061a
JiayiXin-Cindy <77536947+JiayiXin-Cindy@users.noreply.github.com>
2021-09-18 18:52:18 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,61 +2,63 @@ chrome.runtime.onMessage.addListener((msg, sender, response) =>{
     if(msg.command == 'run-complete'){
         document.querySelector('textarea').value = JSON.stringify(msg.data);
         document.querySelector('textarea').style.display = 'block';
-        alert('commands have been run');
     }
 });
 
-
-
-function createCommandObject(){
-
-    var commandsArr = [];
-
-    var commands = document.querySelectorAll('.commands-list .command-item');
-    for (var i=0; i < commands.length; i++) {
-        var itemObj = {};
-        itemObj.type = commands[i].querySelector('select').value;
-        itemObj.one = commands[i].querySelector('.value-1').value;
-        itemObj.two = commands[i].querySelector('.value-2').value;
-        commandsArr.push(itemObj);
-    }
+function createMarker(){
 
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
         var activeTab = tabs[0];
-        var obj = commandArr;
-        chrome.tabs.sendMessage(activeTab.id, {command: "runCommands", data: obj});
+        var obj = document.querySelector('.commands-list .mark');
+        chrome.tabs.sendMessage(activeTab.id, {command: "runMark", data: obj});
+
     
-    });
+});
 }
 
-document.querySelector('.run-command').addEventListener('click', function(){
-    createCommandObject();
+function findMarker(){
+
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        var obj = document.querySelector('.commands-list .find');
+        chrome.tabs.sendMessage(activeTab.id, {command: "runFind", data: obj});
+
+    
+});
+}
+
+
+function deleteMarker(){
+
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        var obj = document.querySelector('.commands-list .delete');
+        chrome.tabs.sendMessage(activeTab.id, {command: "runDelete", data: obj});
+
+    
+});
+}
+
+document.querySelector('.mark').addEventListener('click', function(){
+    createMarker();
 });
 
+document.querySelector('.find').addEventListener('click', function(){
+    findMarker();
+});
 
+document.querySelector('.delete').addEventListener('click', function(){
+    deleteMarker();
+    });
 
-document.querySelector('.new-command').addEventListener('click', function(){
-    var newItem = `<div class="commands-list">
-    <select>
-        <option value="wait">Wait</option>
-        <option value="click">Click</option>
-        <option value="enter">Enter Value</option>
-        <option value="save">Save Value</option>
-    </select>
-    <input class="value-1" placeholder="200ms"/>
-    <input class="value-2" placeholder="Optional"/>
+document.querySelector('.new-marker').addEventListener('click', function(){
+var newItem = `<div class="commands-list">
+<option value="mark">Mark</option>
+<option value="find">Find Marker</option>
+<option value="delete">Delete Marker</option>
 </div>`;
+
 document.querySelector('.command-list').innerHTML+=newItem;
-});
-
-
-chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-    var activeTab = tabs[0];
-    var obj = {};
-
-
-    chrome.tabs.sendMessage(activeTab.id, {command: "runCommands", data: obj});
-
 });
 
 
