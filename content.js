a2f44b2f498e78028598476341be45eefe12325b
Davids048 <90978028+Davids048@users.noreply.github.com>
2021-09-18 13:53:17 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,45 +1,73 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (msg.command == "runCommands"){
-        window.ScraperExt=[];
 
-        var scrapeObj=msg.data;
-        getNextItem(obj,0);
-        window.ScraperExt=[];
-    }
+
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+
+  if(msg.command == "runCommands"){
+    console.log('start commands');
+    window.ScraperExt = [];
+    var scrapeObj = msg.data;
+    getNextItem(scrapeObj, 0);
+
+
+  }
+
+
 });
+
 function getNextItem(obj, index){
-    if (typeof obj[index]!=='undefined'){
-        if(obj[index].type=='click'){
-            clickEvent(obj,index);
-        }
+
+  if(typeof obj[index] !== 'undefined') {
+
+    if(obj[index].type == 'click'){
+      clickEvent(obj, index);
     }
-    else{
-        chrome.runtime.sendMessage({command:"run-complete", data:window.ScraperExt=[]
-    });
+
+    if(obj[index].type == 'wait'){
+      waitEvent(obj, index);
     }
-}
-function waitEvent(selector, obj, index, endItem){
-    var item=obj[index];
-    var waitTime=parseInt(item.one);
-    setTimeout(function(){
-        getNextItem(obj,(index+1));
-    },waitTime);
-}
-function clickEvent(selector, obj, index, endItem){
-    var item=obj[index];
-    document.querySelector(item.one).click();
-    getNextItem(obj,(index+1));
+
+    if(obj[index].type == 'save'){
+      saveEvent(obj, index);
+    }
+
+    if(obj[index].type == 'enter'){
+      enterEvent(obj, index);
+    }
+
+  }else{
+    //send a return ...
+    console.log('run complete');
+    chrome.runtime.sendMessage({command:"run-complete", data: window.ScraperExt});
+  }
+
 }
 
-function saveEvent(selector, obj, index, endItem){
-    var item=obj[index];
-    var value=document.querySelector(item.one).innerText();
-    window.ScraperExt.push(value);
-    getNextItem(obj,(index+1));
+function waitEvent(obj, index){
+  var item = obj[index];
+  var waitTime = parseInt(item.one);
+  setTimeout(function(){
+    getNextItem(obj, (index+1));
+  }, waitTime);
+
 }
-function enterEvent(selector, obj, index, endItem){
-    var item=obj[index];
-    var value=document.querySelector(item.one).value=item.two();
-    window.ScraperExt.push(value);
-    getNextItem(obj,(index+1));
+
+function clickEvent(obj, index){
+  var item = obj[index];
+  document.querySelector(item.one).click();
+  getNextItem(obj, (index+1));
+}
+
+
+function saveEvent(obj, index){
+  var item = obj[index];
+  var value = document.querySelector(item.one).innerText;
+  window.ScraperExt.push(value);
+  getNextItem(obj, (index+1));
+
+}
+function enterEvent(obj, index){
+  var item = obj[index];
+  var value = document.querySelector(item.one).value = item.two;
+  getNextItem(obj, (index+1));
+
 }
