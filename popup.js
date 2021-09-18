babbb27a0c6d8ce9098155f0532427abe1a6c864
JiayiXin-Cindy <77536947+JiayiXin-Cindy@users.noreply.github.com>
2021-09-18 13:31:23 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,51 @@
-document.addEventListener('DOMContentLoaded', function(){
+function createCommandObject(){
+
+    var commandsArr = [];
+
+    var commands = document.querySelectorAll('.commands-list .command-item');
+    for (var i=0; i < commands.length; i++) {
+        var itemObj = {};
+        itemObj.type = commands[i].querySelector('select').value;
+        itemObj.one = commands[i].querySelector('.value-1').value;
+        itemObj.two = commands[i].querySelector('.value-2').value;
+        commandsArr.push(itemObj);
+    }
+
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        var obj = commandArr;
+        chrome.tabs.sendMessage(activeTab.id, {command: "runCommands", data: obj});
+    
+    });
+}
+
+document.querySelector('.run-command').addEventListener('click', function(){
+    createCommandObject();
+});
+
+
+
+document.querySelector('.new-command').addEventListener('click', function(){
+    var newItem = `<div class="commands-list">
+    <select>
+        <option value="wait">Wait</option>
+        <option value="click">Click</option>
+        <option value="enter">Enter Value</option>
+        <option value="save">Save Value</option>
+    </select>
+    <input class="value-1" placeholder="200ms"/>
+    <input class="value-2" placeholder="Optional"/>
+</div>`;
+document.querySelector('.command-list').innerHTML+=newItem;
+});
+
+
+chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    var activeTab = tabs[0];
+    var obj = {};
+
+
+    chrome.tabs.sendMessage(activeTab.id, {command: "runCommands", data: obj});
 
 });
+
