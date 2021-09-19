0b3cfaaf72135b9ba84d1e9b1ca7e673f16244ce
JiayiXin-Cindy <77536947+JiayiXin-Cindy@users.noreply.github.com>
2021-09-18 20:24:14 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,24 +1,74 @@
 
-window.onload =	function () {
-  // If the received message has the expected format...
 
-      // Call the specified callback, passing
-      // the web-page's DOM content as argument
-      // console.log(document.getElementsByTagName("P")[0].innerHTML);
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
-      var x = document.getElementsByTagName("P");
-      // chrome.runtime.sendMessage({"content": x});
+  if(msg.command == "runCommands"){
+    console.log('start commands');
+    window.ScraperExt = [];
+    var scrapeObj = msg.data;
+    getNextItem(scrapeObj, 0);
 
-      var content = [];
 
-        var i;
-        for (i = 0; i < x.length; i++) {
-          // x[i].style.backgroundColor = "red";
-          content.push(x[i].innerHTML);
-          // chrome.runtime.sendMessage({"content": x[i].innerHTML});
+  }
 
-        }
-        chrome.runtime.sendMessage({"content": content});
-      // chrome.runtime.sendMessage({"content": document.getElementsByTagName("P")[0].innerHTML});
 
-};
+});
+
+function getNextItem(obj, index){
+
+  if(typeof obj[index] !== 'undefined') {
+
+    if(obj[index].type == 'click'){
+      clickEvent(obj, index);
+    }
+
+    if(obj[index].type == 'wait'){
+      waitEvent(obj, index);
+    }
+
+    if(obj[index].type == 'save'){
+      saveEvent(obj, index);
+    }
+
+    if(obj[index].type == 'enter'){
+      enterEvent(obj, index);
+    }
+
+  }else{
+    //send a return ...
+    console.log('run complete');
+    chrome.runtime.sendMessage({command:"run-complete", data: window.ScraperExt});
+  }
+
+}
+
+function waitEvent(obj, index){
+  var item = obj[index];
+  var waitTime = parseInt(item.one);
+  setTimeout(function(){
+    getNextItem(obj, (index+1));
+  }, waitTime);
+
+}
+
+function clickEvent(obj, index){
+  var item = obj[index];
+  document.querySelector(item.one).click();
+  getNextItem(obj, (index+1));
+}
+
+
+function saveEvent(obj, index){
+  var item = obj[index];
+  var value = document.querySelector(item.one).innerText;
+  window.ScraperExt.push(value);
+  getNextItem(obj, (index+1));
+
+}
+function enterEvent(obj, index){
+  var item = obj[index];
+  var value = document.querySelector(item.one).value = item.two;
+  getNextItem(obj, (index+1));
+
+}
+
